aee1cd82aa1ce03582b392ebee064db544516626
code77 <kamil.rybczyk@gmail.com>
2014-06-23 14:57:22 -0400

    improved junit for new features in model generation: description and required fields

--- jaxrs-doclet/src/test/resources/fixtures/sample/SubResource.java
+++ jaxrs-doclet/src/test/resources/fixtures/sample/SubResource.java
@@ -3,6 +3,11 @@ package fixtures.sample;
 import javax.ws.rs.*;
 
 public class SubResource {
+    /**
+     * some value
+     */
+    private String value;
+    
     @GET
     @Path("annotated")
     public String sayHello(@QueryParam("name") @DefaultValue("World") String name) {
@@ -12,5 +17,18 @@ public class SubResource {
     @POST
     public int createSub() {
         return 0;
+    }
+
+    public String getValue()  {
+        return value;
+    }
+    
+    /**
+     * this method returns a copy of <code>value</code>
+     * 
+     *  @return copy of a value
+     */
+    public String getValueThroughAnotherMethod()  {
+        return value;
     }
 }
